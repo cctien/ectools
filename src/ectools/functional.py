b3601a4ebe8576c1e2b3ad60c887aad7a89447a4
Chih-chan Tien <chihchan.tien@gmail.com>
2025-06-05 20:24:55 -0500

    being / is

--- src/ectools/functional.py
+++ src/ectools/functional.py
@@ -8,3 +8,11 @@ logger = logging.getLogger(__name__)
 
 def identity[t](x: t) -> t:
     return x
+
+
+def being(a: object, b: object) -> bool:
+    return b is a
+
+
+def not_being(a: object, b: object) -> bool:
+    return b is not a
