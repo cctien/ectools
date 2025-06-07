d2f0a4a317708254879cf4a525a2728cf2d957a8
Chih-chan Tien <chihchan.tien@gmail.com>
2025-06-07 11:18:16 -0500

    ngt

--- src/ectools/functional.py
+++ src/ectools/functional.py
@@ -1,7 +1,10 @@
-from collections.abc import Collection, Mapping
+import functools
 import logging
+from collections.abc import Callable, Sized
+from typing import ParamSpec, TypeVar
 
-from omegaconf import OmegaConf, DictConfig
+T = TypeVar("T")
+P = ParamSpec("P")
 
 logger = logging.getLogger(__name__)
 
@@ -10,9 +13,17 @@ def identity[t](x: t) -> t:
     return x
 
 
-def being(a: object, b: object) -> bool:
-    return b is a
+def ngt(f: Callable[P, bool], /) -> Callable[P, bool]:
+    @functools.wraps(f)
+    def ngt_x(*args, **kwargs):
+        return not f(*args, **kwargs)
+
+    return ngt_x
 
 
-def not_being(a: object, b: object) -> bool:
-    return b is not a
+def est(y: object, x: object, /) -> bool:
+    return x is y
+
+
+def is_empty(x: Sized, /) -> bool:
+    return len(x) == 0
