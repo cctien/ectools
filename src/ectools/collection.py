52f342617513d9ec4bbfa75e71eb11c7a5607aa9
Chih-chan Tien <chihchan.tien@gmail.com>
2025-06-09 13:42:18 -0500

    get function

--- src/ectools/collection.py
+++ src/ectools/collection.py
@@ -1,11 +1,50 @@
 import logging
 import operator
-from collections.abc import Collection, Mapping, Sequence
+from collections.abc import Collection, Hashable, Mapping, Sequence
 from functools import reduce
+from typing import ParamSpec, TypeVar
 
 from omegaconf import DictConfig, OmegaConf
+from plum import dispatch
+
+# from operator import getitem
+
+ex = slice(None, None, None)  # noqa: E741
 
 logger = logging.getLogger(__name__)
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+
+@dispatch
+def getitem(tbl: Sequence, key: int | slice) -> object:
+    return tbl.__getitem__(key)
+
+
+@dispatch
+def getitem(key: int | slice, tbl: Sequence) -> object:
+    return tbl.__getitem__(key)
+
+
+@dispatch
+def getitem(tbl: Mapping, key: Hashable) -> object:
+    return tbl.__getitem__(key)
+
+
+@dispatch
+def getitem(key: Hashable, tbl: Mapping) -> object:
+    return tbl.__getitem__(key)
+
+
+@dispatch
+def get(tbl: dict, key: Hashable, default: object = None) -> object:
+    return tbl.get(key, default)
+
+
+@dispatch
+def get(key: Hashable, tbl: dict, default: object = None) -> object:
+    return tbl.get(key, default)
 
 
 def cnct(*args: Sequence) -> Sequence:
@@ -34,3 +73,29 @@ def mapping_to_dict_rcrs[t](x: Mapping) -> dict:
     if isinstance(x, Mapping):
         return {k: to_dict_from_collection_rcrs(v) for k, v in x.items()}
     raise TypeError(f"Unsupported type for mapping_to_dict_rcrs: {type(x)}. Expected Mapping.")
+
+
+# python -m src.ectools.collection
+if __name__ == "__main__":
+    from functools import partial as prt
+
+    from rich.pretty import pprint
+    from toolz import curry as crr
+    from wadler_lindig import pformat
+
+    tbl = {"a": 1, "b": 2, "c": 3}
+    assert getitem("b", tbl) == 2
+    assert getitem(tbl, "b") == 2
+    pprint(pformat(f"tests passed"))
+
+    assert prt(getitem, "b")(tbl) == 2
+    assert prt(getitem, tbl)("b") == 2
+    pprint(pformat(f"tests passed"))
+
+    assert get(tbl, "r") is None
+    assert get("r", tbl) is None
+    pprint(pformat(f"tests passed"))
+
+    assert prt(get, tbl)("r") is None
+    assert prt(get, "r")(tbl) is None
+    pprint(pformat(f"tests passed"))
