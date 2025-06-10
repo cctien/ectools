561bdd2a72c9aa5ac99a61b5da647e80f863854c
Chih-chan Tien <chihchan.tien@gmail.com>
2025-06-10 11:18:33 -0500

    logsumexp

--- src/ectools/math.py
+++ src/ectools/math.py
@@ -1,5 +1,19 @@
 import math
 
+import numpy as np
+from jaxtyping import Float, Int
+from numpy.typing import ArrayLike
+from scipy import special
+
 
 def isclose_to_int(x: float, /, **kwargs) -> bool:
     return math.isclose(x, round(x), **kwargs)
+
+
+def logsumexp(
+    a: ArrayLike,
+    axis: int | tuple[int] | None = None,
+    b: ArrayLike | None = None,
+    keepdims: bool = False,
+) -> ArrayLike:
+    return special.logsumexp(a, axis=axis, b=b, keepdims=keepdims)
