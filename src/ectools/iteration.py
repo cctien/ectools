91be504ba6dd39b1bd1cb4d31bb62b52a008218e
Chih-chan Tien <chihchan.tien@gmail.com>
2025-09-02 20:17:15 -0500

    items

--- src/ectools/iteration.py
+++ src/ectools/iteration.py
@@ -1,9 +1,14 @@
-from collections.abc import Callable, Iterable, Sequence
+from collections.abc import Callable, ItemsView, Iterable, Mapping, Sequence
 from functools import partial as prt
 from itertools import filterfalse
 from operator import is_
 
 from .collection import len_0
+
+
+def items(x: Mapping) -> ItemsView:
+    return x.items()
+
 
 filter_not_none: Callable[[Iterable], Iterable] = prt(filterfalse, prt(is_, None))
 filter_nonempty: Callable[[Iterable], Iterable] = prt(filterfalse, len_0)
