cbd214455dcd4bb6a8ac8233ef6bddb180e3a7b1
Chih-chan Tien <chihchan.tien@gmail.com>
2025-09-18 17:09:17 -0500

    zps moving fixed

--- src/ectools/__init__.py
+++ src/ectools/__init__.py
@@ -1,3 +1,4 @@
-from .collection import len_0, sole_item, unique_item, zps
+from .collection import len_0, sole_item, unique_item
 from .console import pprint
 from .functional import cmp, cmpl, foldl, foldr
+from .iteration import zps
