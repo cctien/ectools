4271dae173abc5d1e5d97b61cdce14fc457132e0
Chih-chan Tien <chihchan.tien@gmail.com>
2025-09-18 18:01:48 -0500

    better type annotated tuplccmap

--- src/ectools/iteration.py
+++ src/ectools/iteration.py
@@ -1,15 +1,43 @@
-from collections.abc import Callable, ItemsView, Iterable, Mapping, Sequence, Sized
+from collections.abc import Callable, Iterable, Mapping, Sequence, Sized
 from functools import partial as prt
 from itertools import filterfalse
 from operator import is_
-from typing import Any
+from typing import Any, Protocol, overload
 
 from cytoolz import compose as cmp
 
 from .collection import len_0
 
 
-def tuplecmap[S, T](f: Callable[[S], T]) -> Callable[[Iterable[S]], Sequence[T]]:
+@overload
+def tuplecmap[S, T](f: Callable[[S], T]) -> Callable[[Iterable[S]], Sequence[T]]: ...
+
+
+@overload
+def tuplecmap[S1, S2, T](
+    f: Callable[[S1, S2], T],
+) -> Callable[[Iterable[S1], Iterable[S2]], Sequence[T]]: ...
+@overload
+def tuplecmap[S1, S2, S3, T](
+    f: Callable[[S1, S2, S3], T],
+) -> Callable[[Iterable[S1], Iterable[S2], Iterable[S3]], Sequence[T]]: ...
+
+
+@overload
+def tuplecmap[S1, S2, S3, S4, T](
+    f: Callable[[S1, S2, S3, S4], T],
+) -> Callable[[Iterable[S1], Iterable[S2], Iterable[S3], Iterable[S4]], Sequence[T]]: ...
+
+
+@overload
+def tuplecmap[S1, S2, S3, S4, S5, T](
+    f: Callable[[S1, S2, S3, S4, S5], T],
+) -> Callable[
+    [Iterable[S1], Iterable[S2], Iterable[S3], Iterable[S4], Iterable[S5]], Sequence[T]
+]: ...
+
+
+def tuplecmap[T](f: Callable[..., T]) -> Callable[..., Sequence[T]]:
     return cmp(tuple, prt(map, f))
 
 
@@ -19,10 +47,6 @@ def tplmap[T](f: Callable[..., T], *iterables: Iterable) -> Sequence[T]:
 
 def zps(*iterables: Iterable) -> zip:
     return zip(*iterables, strict=True)
-
-
-def items[K, V](x: Mapping[K, V]) -> Iterable[tuple[K, V]]:
-    return x.items()
 
 
 def sorted_keys[K, V](
@@ -39,3 +63,10 @@ filter_nonempty: Callable[[Iterable[Sized]], Iterable[Sized]] = prt(filterfalse,
 filter_not_space: Callable[[Iterable[str]], Iterable[str]] = prt(filterfalse, str.isspace)
 
 # ================================================================
+
+# from operator import methodcaller
+
+# items = methodcaller("items")
+
+# def items[K, V](x: Mapping[K, V]) -> Iterable[tuple[K, V]]:
+#     return x.items()
