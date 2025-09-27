acc9104aee8e8d1c4ddf0db60b23d9941f71f21a
Chih-chan Tien <chihchan.tien@gmail.com>
2025-09-27 13:40:53 -0500

    filer in and filter not in

--- src/ectools/iteration.py
+++ src/ectools/iteration.py
@@ -1,6 +1,6 @@
 from collections.abc import (
     Callable,
-    Generator,
+    Collection,
     Iterable,
     Iterator,
     Mapping,
@@ -10,7 +10,7 @@ from collections.abc import (
 )
 from functools import partial as prt
 from itertools import filterfalse
-from operator import is_
+from operator import contains, is_
 from typing import Any, Protocol, Unpack, overload
 
 from cytoolz import compose as cmp
@@ -104,6 +104,15 @@ def sorted_keys[K, V](
 filter_not_none: Callable[[Iterable[Any]], Iterable[Any]] = prt(filterfalse, prt(is_, None))
 filter_nonempty: Callable[[Iterable[Sized]], Iterable[Sized]] = prt(filterfalse, len_0)
 filter_not_space: Callable[[Iterable[str]], Iterable[str]] = prt(filterfalse, str.isspace)
+
+
+def filter_in[T](c: Collection[T], iterable: Iterable[T]) -> Iterable[T]:
+    return filter(prt(contains, c), iterable)
+
+
+def filter_not_in[T](c: Collection[T], iterable: Iterable[T]) -> Iterable[T]:
+    return filterfalse(prt(contains, c), iterable)
+
 
 # ================================================================
 
