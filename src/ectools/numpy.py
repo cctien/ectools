dd07e0e1fbf04b48d9f044e9914401705601abf2
Chih-chan Tien <chihchan.tien@gmail.com>
2025-09-29 14:38:45 -0500

    np_prng

--- src/ectools/numpy.py
+++ src/ectools/numpy.py
@@ -1,7 +1,19 @@
+from collections.abc import Callable
+
 import numpy as np
+from cytoolz import compose as cmp
 
 
 def np_prng_key(random_seed: int | np.random.SeedSequence | None) -> np.random.SeedSequence:
     if isinstance(random_seed, np.random.SeedSequence):
         return random_seed
     return np.random.SeedSequence(random_seed)
+
+
+def np_prng(random_seed: int | np.random.SeedSequence | None) -> np.random.Generator:
+    return np.random.default_rng(np_prng_key(random_seed))
+
+
+# np_prng: Callable[[int | np.random.SeedSequence | None], np.random.Generator] = cmp(
+#     np.random.default_rng, np_prng_key
+# )
