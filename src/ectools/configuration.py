77bf6f84d491692458a8e94b1172a31ef7df9d5e
Chih-chan Tien <chihchan.tien@gmail.com>
2025-10-02 00:13:49 -0500

    instantiate

--- src/ectools/configuration.py
+++ src/ectools/configuration.py
@@ -113,7 +113,7 @@ def parsed_command_line_arguments(
     return cnfgr
 
 
-def instance(registry: ClassRegistry, configuration: Mapping | str, **kwargs):
+def instantiate(registry: ClassRegistry, configuration: Mapping | str, **kwargs):
     if isinstance(configuration, str):
         configuration = {"cls": configuration}
     _configuration = copy.deepcopy(dict(configuration))
@@ -123,40 +123,40 @@ def instance(registry: ClassRegistry, configuration: Mapping | str, **kwargs):
 # ================================================================================================================================
 
 
-def commandline_args() -> tuple[str | None, Sequence[str]]:
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-c",
-        "--cnfgr",
-        "--cnfgr_file",
-        "--configuration_file",
-        dest="cnfgr",
-        help="Path to the YAML/JSON configuration file",
-        type=str,
-        default=None,
-    )
-    args, unknown = parser.parse_known_args()
-    if any(arg.startswith("-") for arg in unknown):
-        raise ValueError("Additional command line arguments ought not to start with `-`")
-    return args.cnfgr, unknown
+# def commandline_args() -> tuple[str | None, Sequence[str]]:
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument(
+#         "-c",
+#         "--cnfgr",
+#         "--cnfgr_file",
+#         "--configuration_file",
+#         dest="cnfgr",
+#         help="Path to the YAML/JSON configuration file",
+#         type=str,
+#         default=None,
+#     )
+#     args, unknown = parser.parse_known_args()
+#     if any(arg.startswith("-") for arg in unknown):
+#         raise ValueError("Additional command line arguments ought not to start with `-`")
+#     return args.cnfgr, unknown
 
 
-def get_configuration(default: Mapping | object | None = None, strict_level: int = 0) -> DictConfig:
-    """old"""
-    cnfgr_x_programme = dictconfig_created(default)
-    config_filepath, arg_list = commandline_args()
-    cnfgr_x_file = dictconfig_ex_file(config_filepath)
-    cnfgr_x_cli = dictconfig_ex_dotlist(arg_list)
-    merged_config: DictConfig = merged(cnfgr_x_programme, cnfgr_x_file, cnfgr_x_cli)
+# def get_configuration(default: Mapping | object | None = None, strict_level: int = 0) -> DictConfig:
+#     """old"""
+#     cnfgr_x_programme = dictconfig_created(default)
+#     config_filepath, arg_list = commandline_args()
+#     cnfgr_x_file = dictconfig_ex_file(config_filepath)
+#     cnfgr_x_cli = dictconfig_ex_dotlist(arg_list)
+#     merged_config: DictConfig = merged(cnfgr_x_programme, cnfgr_x_file, cnfgr_x_cli)
 
-    if strict_level == 0:
-        return merged_config
-    if strict_level == 1:  # check
-        for key in cnfgr_x_cli.keys():
-            if key not in cnfgr_x_file.keys() | cnfgr_x_programme.keys():
-                message = f"Key {key} in command line arguments not found in file or programme. "
-                warnings.warn(message)
-                breakpoint()
-                raise KeyError(message)
-        return merged_config
-    raise ValueError(f"Unknown strict level {strict_level}")
+#     if strict_level == 0:
+#         return merged_config
+#     if strict_level == 1:  # check
+#         for key in cnfgr_x_cli.keys():
+#             if key not in cnfgr_x_file.keys() | cnfgr_x_programme.keys():
+#                 message = f"Key {key} in command line arguments not found in file or programme. "
+#                 warnings.warn(message)
+#                 breakpoint()
+#                 raise KeyError(message)
+#         return merged_config
+#     raise ValueError(f"Unknown strict level {strict_level}")
