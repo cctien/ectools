1acbb6a429832047804d5f32925bc28444e0c655
Chih-chan Tien <chihchan.tien@gmail.com>
2025-10-08 15:31:58 -0500

    updated

--- src/ectools/__init__.py
+++ src/ectools/__init__.py
@@ -1,4 +1,5 @@
-from .collection import len_0, sole_item, unique_item
-from .console import pprint
-from .functional import cmp, cmpl, foldl, foldr
-from .iteration import zps
+from .collection import len_0 as len_0
+from .collection import sole_item as sole_item
+from .collection import unique_item as unique_item
+from .console import pprint as pprint
+from .iteration import zps as zps
