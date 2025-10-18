12e0aa75e55d477bf59d4ca383a7eb7c7ef85a3e
Chih-chan Tien <chihchan.tien@gmail.com>
2025-10-17 20:09:34 -0500

    fill diagonal

--- src/ectools/numpy.py
+++ src/ectools/numpy.py
@@ -2,6 +2,16 @@ from collections.abc import Sequence
 
 import numpy as np
 
+from .math import number_like
+
+
+def fill_diagonal(
+    x: np.ndarray, value: number_like, *, offset: int = 0, axis1: int = -2, axis2: int = -1
+) -> np.ndarray:
+    x = x.copy()
+    np.diagonal(x, offset=offset, axis1=axis1, axis2=axis2)[...] = value
+    return x
+
 
 def np_prng_key(random_seed: int | np.random.SeedSequence | None) -> np.random.SeedSequence:
     if isinstance(random_seed, np.random.SeedSequence):
